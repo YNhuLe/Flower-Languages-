37d7ac04439d181e8b4efdbb462ca6fd97f0b387
NhuYLe <lenhuy10011996@gmail.com>
2024-11-10 00:28:21 -0500

    first commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="flower" role="img" aria-label="Tense Biceps">
+          {props.flower}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <span>{props.meaning}</span>
+    </div>
+  );
+}
+
+export default Entry;
---
+++ src/flowerpedia.js
@@ -0,0 +1,39 @@
+const flowerpedia = [
+  {
+    id: 1,
+    emoji: "🌹",
+    name: "Rose",
+    meaning:
+      "A symbol of love and passion. Often given on romantic occasions, such as Valentine's Day, the rose can also signify respect or admiration.",
+  },
+  {
+    id: 2,
+    emoji: "🌻",
+    name: "Sunflower",
+    meaning:
+      "Represents positivity, joy, and warmth, as sunflowers turn towards the sun. It is also associated with longevity and loyalty.",
+  },
+  {
+    id: 3,
+    emoji: "🌸",
+    name: "Cherry Blossom",
+    meaning:
+      "Symbolizes the beauty and transience of life. Cherry blossoms are admired in Japan, where they signify renewal and the fleeting nature of existence.",
+  },
+  {
+    id: 4,
+    emoji: "🌼",
+    name: "Daisy",
+    meaning:
+      "A flower symbolizing innocence, purity, and new beginnings. Daisies are often associated with childhood and playful memories.",
+  },
+  {
+    id: 5,
+    emoji: "💐",
+    name: "Bouquet",
+    meaning:
+      "A collection of flowers representing appreciation and celebration. Bouquets are given on various occasions to express congratulations, love, or sympathy.",
+  },
+];
+
+export default flowerpedia;
